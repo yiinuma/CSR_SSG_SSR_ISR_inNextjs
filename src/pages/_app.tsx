3c86df8692560a50691a6efb4a218d132f0ebcab
yiinuma <y.iinuma211@gmail.com>
2022-05-02 22:49:42 +0900

    added reportWebVitals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,33 @@
 import { RecoilRoot } from 'recoil';
-import type { AppProps } from 'next/app';
+import type { AppProps, NextWebVitalsMetric } from 'next/app';
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
 import '../styles/globals.css';
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  switch (metric.name) {
+    case 'FCP':
+      console.log(`FCP: ${Math.round(metric.value * 10) / 10}`);
+      break;
+    case 'LCP':
+      console.log(`LCP: ${Math.round(metric.value * 10) / 10}`);
+      break;
+    case 'TTFB':
+      console.log(`TTFB: ${Math.round(metric.value * 10) / 10}`);
+      break;
+    case 'Next.js-hydration':
+      console.log(
+        `Hydration: ${Math.round(metric.startTime * 10) / 10} -> ${
+          Math.round((metric.startTime + metric.value) * 10) / 10
+        }`,
+      );
+      break;
+    default:
+      break;
+  }
+}
 
 const queryClient = new QueryClient({
   defaultOptions: {
