ef28e664866f897a5c7f669bca1e281684b99176
yiinuma <y.iinuma211@gmail.com>
2022-05-08 18:00:54 +0900

    added BlogPage.test

---
+++ __tests__/BlogDetail.test.tsx
@@ -0,0 +1,89 @@
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen, cleanup } from '@testing-library/react';
+import { getPage } from 'next-page-tester';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import userEvent from '@testing-library/user-event';
+
+const server = setupServer(
+  rest.get('https://jsonplaceholder.typicode.com/posts/', (req, res, ctx) => {
+    const query = req.url.searchParams;
+    const _limit = query.get('_limit');
+    if (_limit === '10') {
+      return res(
+        ctx.status(200),
+        ctx.json([
+          {
+            userId: 1,
+            id: 1,
+            title: 'dummy title 1',
+            body: 'dummy body 1',
+          },
+          {
+            userId: 2,
+            id: 2,
+            title: 'dummy title 2',
+            body: 'dummy body 2',
+          },
+        ]),
+      );
+    }
+  }),
+
+  rest.get('https://jsonplaceholder.typicode.com/posts/1', (_req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        userId: 1,
+        id: 1,
+        title: 'dummy title 1',
+        body: 'dummy body 1',
+      }),
+    );
+  }),
+  rest.get('https://jsonplaceholder.typicode.com/posts/2', (_req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        userId: 2,
+        id: 2,
+        title: 'dummy title 2',
+        body: 'dummy body 2',
+      }),
+    );
+  }),
+);
+
+beforeAll(() => server.listen());
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+afterAll(() => server.close());
+describe(`Blog detail page`, () => {
+  it('render Blog detail page ID 1', async () => {
+    const { page } = await getPage({
+      route: '/posts/1',
+    });
+    render(page);
+    expect(await screen.findByText('dummy title 1')).toBeInTheDocument();
+    expect(screen.getByText('dummy body 1')).toBeInTheDocument();
+  });
+  it('render Blog detail page ID 2', async () => {
+    const { page } = await getPage({
+      route: '/posts/2',
+    });
+    render(page);
+    expect(await screen.findByText('dummy title 2')).toBeInTheDocument();
+    expect(screen.getByText('dummy body 2')).toBeInTheDocument();
+  });
+  it('back to Blog page from Blog detail page', async () => {
+    const { page } = await getPage({
+      route: '/posts/2',
+    });
+    render(page);
+    await screen.findByText('dummy title 2');
+    userEvent.click(screen.getByTestId('back-blog'));
+    expect(await screen.findByRole('heading')).toBeInTheDocument();
+  });
+});
