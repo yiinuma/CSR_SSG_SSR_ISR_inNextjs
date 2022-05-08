cc259559a7fba23a24ac5b05fb84b00bbd32ab44
yiinuma <y.iinuma211@gmail.com>
2022-05-08 17:51:58 +0900

    added BlogPage.test

---
+++ __tests__/BlogPage.test.tsx
@@ -0,0 +1,50 @@
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen, cleanup } from '@testing-library/react';
+import { getPage } from 'next-page-tester';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+const server = setupServer(
+  rest.get('https://jsonplaceholder.typicode.com/posts/', (req, res, ctx) => {
+    const query = req.url.searchParams;
+    const _limit = query.get('_limit');
+    if (_limit === '10') {
+      return res(
+        ctx.status(200),
+        ctx.json([
+          {
+            userId: 1,
+            id: 1,
+            title: 'dummy title 1',
+            body: 'dummy body 1',
+          },
+          {
+            userId: 2,
+            id: 2,
+            title: 'dummy title 2',
+            body: 'dummy body 2',
+          },
+        ]),
+      );
+    }
+  }),
+);
+
+beforeAll(() => server.listen());
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+afterAll(() => server.close());
+
+describe(`Blog page`, () => {
+  it('pre-fetched by getStaticProps', async () => {
+    const { page } = await getPage({
+      route: '/blog-page',
+    });
+    render(page);
+    expect(await screen.findByRole('heading')).toBeInTheDocument();
+    expect(screen.getByText('dummy title 1')).toBeInTheDocument();
+    expect(screen.getByText('dummy title 2')).toBeInTheDocument();
+  });
+});
