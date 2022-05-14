e8304c55def774410bf109219084821c7ef26e46
yiinuma <y.iinuma211@gmail.com>
2022-05-14 16:32:17 +0900

    added PrimaryButton.stories

---
+++ src/components/button/PrimaryButton.stories.tsx
@@ -0,0 +1,15 @@
+import type { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+
+import { PrimaryButton } from './PrimaryButton';
+
+// eslint-disable-next-line storybook/story-exports
+export default {
+  title: 'PrimaryButton',
+  component: PrimaryButton,
+} as ComponentMeta<typeof PrimaryButton>;
+
+type Story = ComponentStoryObj<typeof PrimaryButton>;
+
+export const Default: Story = {
+  args: { children: 'Primary' },
+};
